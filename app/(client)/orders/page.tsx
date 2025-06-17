dc7268bc142355811c1ee3efac8356c8340f3973
Plamen Kostov <plamenkostov81@gmail.com>
2025-06-17 14:41:12 +0300

    orders page created, route  and beckendClient created

---
+++ app/(client)/orders/page.tsx
@@ -0,0 +1,19 @@
+import Container from "@/components/Container";
+
+import React from "react";
+
+const OrdersPage = async () => {
+  
+
+  return (
+    <div>
+      <Container className="py-10">
+      Ордер паге
+      </Container>
+    </div>
+  );
+};
+
+export default OrdersPage;
+
+
