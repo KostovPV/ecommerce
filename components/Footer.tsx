f10188b66b79366908f411aadc987c322a1ae9cd
Plamen Kostov <plamenkostov81@gmail.com>
2025-06-18 11:53:14 +0300

    Footer edited

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,30 +1,3 @@
-// import React from "react";
-// import Container from "./Container";
-// import Image from "next/image";
-// import payment from "@/images/payment.png";
-
-// const Footer = () => {
-//   return (
-//     <div className="bg-lightBg text-sm">
-//       <Container className="py-5">
-//         <footer className="flex items-center justify-between">
-//           <p className="text-gray-500">
-//             Copyright © 2024{" "}
-//             <span className="text-darkBlue font-semibold">
-//                 Plan B Web services
-//                 </span> all
-//             rights reserved.
-//           </p>
-//           <Image src={payment} alt="payment" className="w-64 object-cover" />
-//         </footer>
-//       </Container>
-//     </div>
-//   );
-// };
-
-// export default Footer;
-
-
 import React from "react";
 import Container from "./Container";
 import Image from "next/image";
@@ -34,10 +7,10 @@ const Footer = () => {
   return (
     <div className="bg-lightBg text-sm">
       <Container className="py-5">
-        <footer className="flex items-center justify-between">
+        <footer className="flex flex-col md:flex-row items-center justify-between gap-4 text-center md:text-left">
           <p className="text-gray-500">
-            Copyright © 2024{" "}
-            <span className="text-darkBlue font-semibold">reactBD</span> all
+            Copyright © 2025{" "}
+            <span className="text-darkBlue font-semibold"> Plan B Web services</span> all
             rights reserved.
           </p>
           <Image src={payment} alt="payment" className="w-64 object-cover" />
